ba1ae58fa3101b8387c5736a7be4cacde428b473
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2023-11-20 17:36:28 +0500

    add link

--- media/views.py
+++ media/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def main(request):
-    return render(request, 'main.html', {})
+    posts = Post.objects.all()
+    for post in posts:
+        print(post.image)
+    return render(request, 'main.html', {'posts':posts})
+
+def detail(request, pk):
+    post = Post.objects.get(id=int(pk))
+    return render(request, 'singlepost.html', {'post':post})
